c710178f317deb37ad89fee1143b074af814e2f3
kiranisme1 <kpatel1357911@gmail.com>
2026-05-25 14:26:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev
-export default defineConfig({
-  plugins: [react()],
-  base: '/Calc-Quest/',
-})
+name: Deploy Game
+on:
+  push:
+    branches: [ main ]
+permissions:
+  contents: read
+  pages: write
+  id-token: write
+concurrency:
+  group: "pages"
+  cancel-in-progress: false
+jobs:
+  deploy:
+    environment:
+      name: github-pages
+      url: ${{ steps.deployment.outputs.page_url }}
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - name: Unzip Uploaded Game Files
+        run: |
+          if [ -f "./clean-game-upload.zip" ]; then
+            unzip -o ./clean-game-upload.zip -d ./
+            rm ./clean-game-upload.zip
+          fi
+      - uses: actions/setup-node@v4
+        with:
+          node-version: 20
+      - run: npm install
+      - run: npm run build
+      - uses: actions/configure-pages@v5
+      - uses: actions/upload-pages-artifact@v3
+        with:
+          path: './dist'
+      - id: deployment
+        uses: actions/deploy-pages@v4
